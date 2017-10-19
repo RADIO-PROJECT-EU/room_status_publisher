460bc4985c876dc5a69ca58d5f3e906f7066979a
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-10-19 16:21:48 +0300

    Prepared code for the final version of the TWG-S&C TV watching recording

--- src/room_status.py
+++ src/room_status.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python
+import json
 import rospy
 import paho.mqtt.client as mqtt
 from room_status_publisher.msg import RoomStatusMsg
+from std_msgs.msg import Bool
 
-ros_pub = None
 stopic = ""
+tv_pub = None
+ros_pub = None
+chair_tv_location = ""
+chair_object_name = ""
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     global stopic
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe(stopic)
+    print client.subscribe(stopic, 0)
+    print "Subscribing to " + stopic
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print "Subscription successful!"
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    global ros_pub
-    print msg.payload
+    global ros_pub, tv_pub, chair_object_name, chair_tv_location
+    j = json.loads(msg.payload)
+    b = Bool()
+    if j["object"] == chair_object_name and j["location"] == chair_tv_location:
+        b.data = True
+    else:
+        b.data = False
+
+    tv_pub.publish(b)
 
     #m = RoomStatusMsg()
     #m.header.stamp = rospy.Time.now()
@@ -28,23 +41,29 @@ def on_message(client, userdata, msg):
     #print(msg.topic+" "+str(msg.payload))
 
 
-
-
 def init():
-    global ros_pub, stopic
+    global ros_pub, stopic, tv_pub, chair_tv_location, chair_object_name
+    # ROS stuff
+    rospy.init_node('room_status_publisher')
+    stopic = rospy.get_param("~sub_topic","apps/localization/relative")
+    #ptopic = rospy.get_param("pub_topic","~room_status")
+    tvtopic = rospy.get_param("~tv_topic","~tv_chair")
+    chair_object_name = rospy.get_param("~chair_object_name","chair")
+    chair_tv_location = rospy.get_param("~chair_tv_location","tv")
+
+    #ros_pub = rospy.Publisher(ptopic, RoomStatusMsg, queue_size=1)
+
+    tv_pub = rospy.Publisher(tvtopic, Bool, queue_size=1)
+
+    # MQTT stuff
     client = mqtt.Client()
     client.on_connect = on_connect
+    client.on_subscribe = on_subscribe
     client.on_message = on_message
 
-    client.username_pw_set("application",password="@PpL3c@tI0n")
-
-    client.connect("172.21.13.170", 1883, 60)
-    rospy.init_node('room_status_publisher')
-    stopic = rospy.get_param("sub_topic","twg/relative/room_status_mqtt")
-    ptopic = rospy.get_param("pub_topic","room_status_publisher/room_status")
-
-    ros_pub = rospy.Publisher(ptopic, RoomStatusMsg, queue_size=1)
-    print "node init"
+    #client.username_pw_set("application",password="@PpL3c@tI0n")
+    #client.connect("150.140.187.126", 1883, 60)
+    client.connect("localhost", 1883, 60)
 
     # Blocking call that processes network traffic, dispatches callbacks and
     # handles reconnecting.
