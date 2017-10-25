2fa27a745123ce1e83245a459c5bd178a807e341
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-10-25 16:28:26 +0300

    Added specific check for the tv-chair

--- src/room_status.py
+++ src/room_status.py
@@ -26,19 +26,12 @@ def on_message(client, userdata, msg):
     global ros_pub, tv_pub, chair_object_name, chair_tv_location
     j = json.loads(msg.payload)
     b = Bool()
-    if j["object"] == chair_object_name and j["location"] == chair_tv_location:
+    if j["object"].lower() == chair_object_name and j["location"].lower() == chair_tv_location:
         b.data = True
     else:
         b.data = False
 
     tv_pub.publish(b)
-
-    #m = RoomStatusMsg()
-    #m.header.stamp = rospy.Time.now()
-    #m.name = msg.name
-    #m.things = mgs.things
-    #ros_pub.publish(m)
-    #print(msg.topic+" "+str(msg.payload))
 
 
 def init():
@@ -46,10 +39,17 @@ def init():
     # ROS stuff
     rospy.init_node('room_status_publisher')
     stopic = rospy.get_param("~sub_topic","apps/localization/relative")
-    #ptopic = rospy.get_param("pub_topic","~room_status")
+    ptopic = rospy.get_param("~pub_topic","~room_status")
     tvtopic = rospy.get_param("~tv_topic","~tv_chair")
-    chair_object_name = rospy.get_param("~chair_object_name","chair")
-    chair_tv_location = rospy.get_param("~chair_tv_location","tv")
+    chair_object_name = rospy.get_param("~chair_object_name", "chair")
+    chair_tv_location = rospy.get_param("~chair_tv_location", "tv")
+    chair_object_name = chair_object_name.lower()
+    chair_tv_location = chair_tv_location.lower()
+    ip = rospy.get_param("~ip", "150.140.187.125")
+    port = rospy.get_param("~port", 1883)
+    timeout = rospy.get_param("~timeout", 60)
+    username = rospy.get_param("~username", "application")
+    password = rospy.get_param("~password", "@PpL3c@tI0n")
 
     #ros_pub = rospy.Publisher(ptopic, RoomStatusMsg, queue_size=1)
 
@@ -61,9 +61,8 @@ def init():
     client.on_subscribe = on_subscribe
     client.on_message = on_message
 
-    #client.username_pw_set("application",password="@PpL3c@tI0n")
-    #client.connect("150.140.187.126", 1883, 60)
-    client.connect("localhost", 1883, 60)
+    client.username_pw_set(username, password=password)
+    client.connect(ip, port, timeout)
 
     # Blocking call that processes network traffic, dispatches callbacks and
     # handles reconnecting.
